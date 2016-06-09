c6db3b16053ea9579dfc2c03550c49d612d8eaee
Omar Sahyoun <osahyoun@gmail.com>
2016-06-09 13:47:33 +0100

    additions

--- public/application.js
+++ public/application.js
@@ -8,6 +8,7 @@ var socket = io();
 
 socket.on('actions', function(msg){
   var data = JSON.parse(msg);
+  console.log(data);
   var actionHtml = $(template(data));
 
   $actions.prepend(actionHtml);
