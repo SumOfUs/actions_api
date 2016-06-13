89a86f2cbdd93c1c9e2d510d484fd9a717f3f286
Omar Sahyoun <osahyoun@gmail.com>
2016-06-13 10:49:36 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ var redis = require("redis").createClient(redisOptions);
 
 redis.subscribe('champaign:actions');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
